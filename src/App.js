d5c14ab7337bd64794adf4e4651651a7f56c4278
owsy123 <owais.sohail3@gmail.com>
2020-07-23 00:02:05 +0500

    summary

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// import "../node_modules/react-dat-gui/build/react-dat-gui.css";
+
+import FluidAnimation from "./react-fluid-animation";
+import random from "random";
+
+const defaultConfig = {
+  textureDownsample: 1,
+  densityDissipation: 0.98,
+  velocityDissipation: 0.99,
+  pressureDissipation: 0.8,
+  pressureIterations: 25,
+  curl: 30,
+  splatRadius: 0.005
+};
+
+export default class App extends Component {
+  state = {
+    config: {
+      ...defaultConfig
+    }
+  };
+
+  componentDidMount() {
+    this._reset();
+  }
+
+  render() {
+    const { config } = this.state;
+
+    return (
+      <div
+        style={{
+          height: "100vh"
+        }}
+      >
+        <FluidAnimation config={config} animationRef={this._animationRef} />
+
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 1,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            padding: "1em",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#fff",
+            fontFamily: 'Quicksand, "Helvetica Neue", sans-serif',
+            pointerEvents: "none"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <h1
+            style={{
+              fontSize: "3em",
+              textShadow: "2px 2px 8px rgba(0, 0, 0, 0.5)"
+            }}
+          >
+            Click and Move Mouse
+          </h1>
+        </div>
 
-export default App;
+    
+      </div>
+    );
+  }
+
+  _animationRef = ref => {
+    this._animation = ref;
+    this._reset();
+  };
+  _reset() {
+    if (this._animation) {
+      this._animation.addRandomSplats(random.int(100, 180));
+    }
+  }
+}
